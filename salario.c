13f1e6dd486b091f6919e7dabf53bad058bd3942
Gabriel Borralho <39606678+gabrielrhcp@users.noreply.github.com>
2019-10-17 21:18:55 -0300

    Update salario.c
    melhora na atribuição de variáveis

--- salario.c
+++ salario.c
@@ -1,13 +1,19 @@
+/* 
+Programa que lê o número de um funcionário, seu número de horas trabalhadas, 
+o valor que recebe por hora e calcula o salário desse funcionário. 
+A seguir, mostre o número e o salário do funcionário, com duas casas decimais.
+*/
+
 #include <stdio.h>
 
 int main(){
     int num,ht;
-    float sal,pag;
+    float vph,sal;
     scanf("%d",&num);
     scanf("%d",&ht);
-    scanf("%f",&sal);
-    pag=sal*ht;
+    scanf("%f",&vph);
+    sal=sal*ht;
     printf("NUMBER = %d\n",num);
-    printf("SALARY = U$ %.2f\n",pag);
+    printf("SALARY = U$ %.2f\n",sal);
     return 0;
 }
